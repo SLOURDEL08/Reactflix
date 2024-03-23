d0cc1880e30d67d0cbfcbaf57409d8f972f62ba6
SebLOURDEL <seb.lourdel8@gmail.com>
2024-03-23 19:15:13 +0100

    app.js correction

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="app">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} index />
+          <Route element={<Home />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
         </Routes>
       </div>
